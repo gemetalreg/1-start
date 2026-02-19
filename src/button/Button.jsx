728750f2b4e7ef0fc150d805d4b7269c573b3eb5
Andrei <itunereg@gmail.com>
2026-02-19 09:56:58 +0300

    react

---
+++ src/button/Button.jsx
@@ -0,0 +1,8 @@
+import "./Button.css";
+
+
+function Button() {
+    return <button className="button">Искать</button>
+}
+
+export default Button;
---
+++ src/header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css";
+
+
+function Header() {
+    return <h1 className="header">Поиск</h1>
+}
+
+export default Header;
